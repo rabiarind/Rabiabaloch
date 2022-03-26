e350afb3165bf6d885f42964264169f7f728a5dd
rabiabaloch <rabiagullrind@gmail.com>
2022-03-26 14:24:04 +0500

    my third commitr

---
+++ testproject/src/testproject/firstproject.java
@@ -0,0 +1,15 @@
+package testproject;
+
+public class firstproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello word");
+		int a =9;
+		int b= 8;
+int c;
+c=a+b;
+System.out.println(c);
+	}
+
+}
